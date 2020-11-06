c74ff52804c43653a26872bd6d9bc208654baa05
solin <solin.li@links-e.com>
2020-11-06 20:56:08 +0800

    日志输出基础配置完成

--- src/main/java/cn/solin/testLog4j2/controller/TestController.java
+++ src/main/java/cn/solin/testLog4j2/controller/TestController.java
@@ -13,7 +13,9 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        log.info("test:接收到请求了");
+        log.info("test:info接收到请求了");
+        log.warn("test:warn接收到请求了");
+        log.error("test:error接收到请求了");
         return "aaa";
     }
 }
